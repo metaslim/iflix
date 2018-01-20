b0c1a09e95af4fb88cfcc3f047356672c93d7d12
Welly <welly@wellys-mbp.home>
2018-01-20 15:35:55 +1100

    implment rate controller

--- api/controllers/rateController.js
+++ api/controllers/rateController.js
@@ -5,25 +5,36 @@ var Rate = mongoose.model('Rate');
 
 exports.create_a_rate = function(req, res) {
   user = User.findOne(
-    { _id: req.body.user_id }
-  )
+    { _id: req.body.user_id },
+    '_id',
+    function (err, person) {
+      if (err)
+        return res.status(500).send(err);
 
-  if(!user) {
-    res.send("User ID " + req.body.user_id + " not found");
-  }
+      if (!user)
+        return res.status(404).send("No user found.");
+    }
+  );
 
   content = Content.findOne(
-    { _id: req.body.content_id }
-  )
+    { _id: req.body.content_id },
+    '_id',
+    function (err, content) {
+      if (err)
+        return res.status(500).send(err);
 
-  if(!content) {
-    res.send("Content ID " + req.body.content_id + " not found");
-  }
+      if (!user)
+        return res.status(404).send("No Content found.");
+    }
+  );
+
+  if (req.body.rating > 5 && req.body.rating < 1)
+      return res.status(500).send("Invalid Rating.");
 
   var new_rate = new Rate(
     {
       user: user._id,
-      content: content_id,
+      content: content._id,
       rating: req.body.rating
     }
   );
