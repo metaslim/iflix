6448dd1022cf9d27bb372c18464e405a042fc635
Welly <welly@wellys-mbp.home>
2018-01-20 16:59:01 +1100

    implementing rating api wiht async package instead of nested callback

--- api/controllers/rateController.js
+++ api/controllers/rateController.js
@@ -1,47 +1,85 @@
 'use strict';
 
+var async = require("async");
 var mongoose = require('mongoose');
 var Rate = mongoose.model('Rate');
+var User = mongoose.model('User');
+var Content = mongoose.model('Content');
 
 exports.create_a_rate = function(req, res) {
-  user = User.findOne(
-    { _id: req.body.user_id },
-    '_id',
-    function (err, person) {
-      if (err)
-        return res.status(500).send(err);
+  async.parallel({
+    user: function(callback) {
+      if (!mongoose.Types.ObjectId.isValid(req.query.user_id)) {
+        return callback('Invalid used_id');
+      }
 
-      if (!user)
-        return res.status(404).send("No user found.");
+      console.log(req.query.user_id)
+      User.findOne(
+        { _id: mongoose.Types.ObjectId(req.query.user_id) },
+        function(err, result) {
+          if (err) {
+            callback(err);
+          }
+          else {
+            if (!result) {
+              return callback('No user found');
+            }
+
+            callback(null, result);
+          }
+        }
+      )
+    },
+    content: function(callback) {
+      if (!mongoose.Types.ObjectId.isValid(req.query.user_id)) {
+        return callback('Invalid content_id');
+      }
+
+      Content.findOne(
+        { _id: mongoose.Types.ObjectId(req.query.content_id) },
+        function(err, result) {
+          if (err) {
+            callback(err);
+          }
+          else {
+            if (!result) {
+              return callback('No content found');
+            }
+
+            callback(null, result);
+          }
+        }
+      )
+    },
+    rating: function(callback) {
+      if (req.query.rating > 5 || req.query.rating < 1) {
+        return callback('Invalid rating');
+      }
+
+      callback(null, req.query.rating);
+    },
+  },
+  function(err, results) {
+    if(err) {
+      return res.send(err);
     }
-  );
 
-  content = Content.findOne(
-    { _id: req.body.content_id },
-    '_id',
-    function (err, content) {
-      if (err)
-        return res.status(500).send(err);
+    if(results) {
+      var new_rate = new Rate(
+        {
+          user: results.user._id,
+          content: results.content._id,
+          rating: results.rating,
+        }
+      );
 
-      if (!user)
-        return res.status(404).send("No Content found.");
+      new_rate.save(function(err, rate) {
+        if (err)
+          res.send(err);
+
+        res.json({msg : "Thanks for the rate"});
+      });
     }
+  }
   );
-
-  if (req.body.rating > 5 && req.body.rating < 1)
-      return res.status(500).send("Invalid Rating.");
-
-  var new_rate = new Rate(
-    {
-      user: user._id,
-      content: content._id,
-      rating: req.body.rating
-    }
-  );
-
-  new_rate.save(function(err, rate) {
-    if (err)
-      res.send(err);
-    res.json({msg : "Thanks for the rate"});
-  });
 };
