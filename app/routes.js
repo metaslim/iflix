bc4bcef89cd8676ff05b5876deea257c0645300e
w <wellysalim@donesafe.com>
2018-02-08 17:03:27 +1100

    make it tidier

--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,8 @@
 'use strict';
+const rate = require('./controllers/rate/routes');
+const content = require('./controllers/content/routes');
 
 module.exports = (app) => {
-  const rate = require('./controllers/rate/routes');
-  const content = require('./controllers/content/routes');
-
   app.use('/rating', rate)
   app.use('/content', content)
 };
